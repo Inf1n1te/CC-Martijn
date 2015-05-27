18ededbb2baab41bcc17d717d7abc6c865d99348
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-05-27 09:33:32 +0200

    \/o\/

--- src/pp/block4/cc/iloc/CalcCompiler.java
+++ src/pp/block4/cc/iloc/CalcCompiler.java
@@ -1,13 +1,13 @@
 package pp.block4.cc.iloc;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import pp.block4.cc.ErrorListener;
 import pp.iloc.Simulator;
-import pp.iloc.model.Op;
-import pp.iloc.model.OpCode;
-import pp.iloc.model.Operand;
-import pp.iloc.model.Program;
+import pp.iloc.model.*;
 
 /**
  * Compiler from Calc.g4 to ILOC.
@@ -15,7 +15,10 @@ import pp.iloc.model.Program;
 public class CalcCompiler extends CalcBaseListener {
 	/** Program under construction. */
 	private Program prog;
+
 	// Attribute maps and other fields
+	private ParseTreeProperty<Reg> registers;
+	private int numberOfRegisters;
 
 	/** Calls the compiler, and simulates and prints the compiled program. */
 	public static void main(String[] args) {
@@ -47,9 +50,7 @@ public class CalcCompiler extends CalcBaseListener {
 		ParseTree tree = parser.complete();
 		if (listener.hasErrors()) {
 			System.out.printf("Parse errors in %s:%n", text);
-			for (String error : listener.getErrors()) {
-				System.err.println(error);
-			}
+			listener.getErrors().forEach(System.err::println);
 		} else {
 			result = compile(tree);
 		}
@@ -58,8 +59,57 @@ public class CalcCompiler extends CalcBaseListener {
 
 	/** Compiles a given Calc-parse tree into an ILOC program. */
 	public Program compile(ParseTree tree) {
-		// TODO Fill in
-		throw new UnsupportedOperationException("Fill in");
+		prog = new Program();
+		registers = new ParseTreeProperty<>();
+		numberOfRegisters = 0;
+
+		addNewRegister(tree);
+		new ParseTreeWalker().walk(this, tree);
+		emit(OpCode.out, new Str(""), new Reg("r_" + (numberOfRegisters - 1)));
+		return prog;
+	}
+
+	private void addNewRegister(ParseTree tree) {
+		registers.put(tree, new Reg("r_" + numberOfRegisters));
+		numberOfRegisters++;
+	}
+
+	@Override
+	public void exitPar(@NotNull CalcParser.ParContext ctx) {
+		addNewRegister(ctx);
+		emit(OpCode.i2i, registers.get(ctx.expr()), registers.get(ctx));
+	}
+
+	@Override
+	public void exitMinus(@NotNull CalcParser.MinusContext ctx) {
+		addNewRegister(ctx);
+
+		Reg expr = registers.get(ctx.expr());
+		emit(OpCode.rsubI, expr, new Num(0), registers.get(ctx));
+	}
+
+	@Override
+	public void exitNumber(@NotNull CalcParser.NumberContext ctx) {
+		addNewRegister(ctx);
+		emit(OpCode.loadI, new Num(Integer.parseInt(ctx.getText())), registers.get(ctx));
+	}
+
+	@Override
+	public void exitTimes(@NotNull CalcParser.TimesContext ctx) {
+		addNewRegister(ctx);
+
+		Reg expr0 = registers.get(ctx.expr(0));
+		Reg expr1 = registers.get(ctx.expr(1));
+		emit(OpCode.mult, expr0, expr1, registers.get(ctx));
+	}
+
+	@Override
+	public void exitPlus(@NotNull CalcParser.PlusContext ctx) {
+		addNewRegister(ctx);
+
+		Reg expr0 = registers.get(ctx.expr(0));
+		Reg expr1 = registers.get(ctx.expr(1));
+		emit(OpCode.add, expr0, expr1, registers.get(ctx));
 	}
 
 	/** Constructs an operation from the parameters
