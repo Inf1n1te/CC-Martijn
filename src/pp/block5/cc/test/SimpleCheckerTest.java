f40c4bc84a698384785f163af4f1da3c1cc7a739
Inf1n1te <t.kerkhoven@student.utwente.nl>
2015-05-29 14:11:34 +0200

    \/o\/

--- src/pp/block5/cc/test/SimpleCheckerTest.java
+++ src/pp/block5/cc/test/SimpleCheckerTest.java
@@ -15,21 +15,37 @@ import static org.junit.Assert.fail;
 
 @SuppressWarnings("javadoc")
 public class SimpleCheckerTest {
-	private final static String BASE_DIR = "src/pp/block5/cc/sample";
+	private final static String BASE_DIR = "pp/block5/cc/sample";
 	private final static String EXT = ".pascal";
 	private final SimplePascalCompiler compiler = SimplePascalCompiler
 			.instance();
 
 	@Test
-	public void testBasic() throws IOException, ParseException {
+	public void testBasicTypes() throws IOException, ParseException {
 		ParseTree tree = parse("basic");
 		Result result = check(tree);
 		ParseTree body = tree.getChild(3).getChild(1);
 		ParseTree assX = body.getChild(1);
-		assertEquals(assX.getChild(2), result.getEntry(body));
-		assertEquals(assX.getChild(2), result.getEntry(assX));
 		assertEquals(Type.INT, result.getType(assX.getChild(0)));
 		assertEquals(Type.INT, result.getType(assX.getChild(2)));
+	}
+
+	@Test
+	public void testBasicEntries() throws IOException, ParseException {
+		ParseTree tree = parse("basic");
+		Result result = check(tree);
+		ParseTree body = tree.getChild(3).getChild(1);
+		ParseTree assX = body.getChild(1);
+		assertEquals(assX.getChild(2), result.getEntry(assX));
+		assertEquals(assX.getChild(2), result.getEntry(body));
+	}
+
+	@Test
+	public void testBasicOffsets() throws IOException, ParseException {
+		ParseTree tree = parse("basic");
+		Result result = check(tree);
+		ParseTree body = tree.getChild(3).getChild(1);
+		ParseTree assX = body.getChild(1);
 		assertEquals(0, result.getOffset(assX.getChild(0)));
 	}
 
